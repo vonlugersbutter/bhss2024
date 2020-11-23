c838d56c673b95ceab23e37d51677f6721abce31
vonlugersbutter <ayeshaumair10@gmail.com>
2020-11-22 22:22:03 -0500

    fixed about page

--- public/js/script.js
+++ public/js/script.js
@@ -9,3 +9,70 @@ $(function() {
 $(function() {
     $("#teamcards").load("/teamcards.html");
 });
+
+if (!window.getComputedStyle) {
+    // Fallback for obsolete IE
+    window.getComputedStyle = function(e) {
+        return e.currentStyle;
+    };
+}
+
+function grace() {
+    var x = document.getElementById("grace");
+    x.classList.toggle("beef");
+  }
+
+  function alexx() {
+    var x = document.getElementById("alexx");
+    x.classList.toggle("beef");
+  }
+
+  function emily() {
+    var x = document.getElementById("emily");
+    x.classList.toggle("beef");
+  }
+
+  function alexp() {
+    var x = document.getElementById("alexp");
+    x.classList.toggle("beef");
+  }
+
+  function ahad() {
+    var x = document.getElementById("ahad");
+    x.classList.toggle("beef");
+  }
+
+  function michael() {
+    var x = document.getElementById("michael");
+    x.classList.toggle("beef");
+  }
+
+  function ishaq() {
+    var x = document.getElementById("ishaq");
+    x.classList.toggle("beef");
+  }
+
+  function nyah() {
+    var x = document.getElementById("nyah");
+    x.classList.toggle("beef");
+  }
+
+  function esha() {
+    var x = document.getElementById("esha");
+    x.classList.toggle("beef");
+  }
+
+  function bigboi() {
+    var x = document.getElementById("bigboi");
+    x.classList.toggle("beef");
+  }
+
+  function dim() {
+    var x = document.getElementById("dim");
+    x.classList.toggle("beef");
+  }
+
+  function trisha() {
+    var x = document.getElementById("trisha");
+    x.classList.toggle("beef");
+  }
