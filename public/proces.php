7014b0ed38e0967844396c363e407b1d5e63f773
vonlugersbutter <ayeshaumair10@gmail.com>
2021-02-13 13:26:26 -0500

    eheh

--- public/proces.php
+++ public/proces.php
@@ -1,14 +1,12 @@
+
 <?php
-    $keys = array('confession');
-    $csv_line = array();
-    foreach($keys as $key){
-        array_push($csv_line,'' . $_GET[$key]);
-    }
-    $fname = 'file_to_write_to.csv';
-    $csv_line = implode(',',$csv_line);
-    if(!file_exists($fname)){$csv_line = "\r\n" . $csv_line;}
-    $fcon = fopen($fname,'a');
-    $fcontent = $csv_line;
-    fwrite($fcon,$csv_line);
-    fclose($fcon);
+
+if(isset($_POST['confession']))
+{
+$data=$_POST['confession'];
+
+$fp = fopen('data.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+}
 ?>
