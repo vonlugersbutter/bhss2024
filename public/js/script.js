3bd3f1c55b8e9803789b7f687bde156abd72770e
vonlugersbutter <ayeshaumair10@gmail.com>
2021-07-25 19:53:02 -0400

    uwu

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,15 @@
+const setTheme = (theme) => {
+  document.documentElement.className = theme;
+  localStorage.setItem('theme', theme);
+}
+
+document.getElementById('theme-select').addEventListener('change', function() {
+  setTheme(this.value);
+});
+
 const getTheme = () => {
   const theme = localStorage.getItem('theme');
   theme && setTheme(theme);
 }
  
 getTheme();
-
-
-const setTheme = theme => document.documentElement.className = theme;
- 
-document.getElementById('theme-select').addEventListener('change', function() {
-  setTheme(this.value);
-  localStorage.setItem('theme', theme);
-});
-
