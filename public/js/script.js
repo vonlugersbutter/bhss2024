6d0641aee29d9a003de4659a7845153b3512bc25
vonlugersbutter <ayeshaumair10@gmail.com>
2021-08-15 01:04:41 -0400

    added conditional viewing of private posts

--- public/js/script.js
+++ public/js/script.js
@@ -16,12 +16,47 @@ document.addEventListener('DOMContentLoaded', e => {
   });
 });
 
+//listen for auth status changes
+auth.onAuthStateChanged(user => {
+  if (user) {
+    // get data
+    db.collection('forumposts').get().then(snapshot => {
+      setupPosts(snapshot.docs);
+    });
+  } else {
+    setupPosts([]);
+  }
+})
+
 // log out
 
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
     e.preventDefault();
-    auth.signOut().then(() => {
-      console.log('user signed out')
-    });
+    auth.signOut();
 });
+
+const postList = document.querySelector('.forumposts');
+const setupPosts = (data) => {
+  
+  if (data.length) {
+  
+  let html = '';
+
+  data.forEach(doc => {
+    const post = doc.data();
+    console.log(post)
+    const li = `
+        <div class="post"
+        <p><b>${post.Name}</b></p>
+        <p>${post.Body}</p>
+        </div>
+    `;
+    html += li
+  });
+
+  postList.innerHTML = html;
+  } else {
+    postList.innerHTML = '<p>You have been successfully logged out. You may navigate to another page, or you can sign in again using the <a href="myaccount.html">My Account</a> page.</p>'
+  }
+}
