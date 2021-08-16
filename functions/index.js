ace83288c151f7e13fb44e05416425ea91ecbffc
vonlugersbutter <ayeshaumair10@gmail.com>
2021-08-15 23:34:34 -0400

    added admin rules

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,18 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.addAdminRole = functions.https.onCall((data, context) => {
+    // get user and add custom claim (full-user)
+    return admin.auth().getUserByEmail(data.email).then(user => {
+        return admin.auth().setCustomUserClaims(user.uid, {
+            admin: true
+        });
+    }).then(() => {
+        return {
+            message: `Success! ${data.email} has been verified as a user and now has full access to the private pages.`
+        }
+    }).catch(err => {
+        return err;
+    });
+});
